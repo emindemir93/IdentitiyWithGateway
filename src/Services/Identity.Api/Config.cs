23a659fc37ee823bad00aa03bb44293228acccda
Emin Demir <demir.emin@outlook.com.tr>
2022-05-31 00:15:09 +0300

    fixes

--- src/Services/Identity.Api/Config.cs
+++ src/Services/Identity.Api/Config.cs
@@ -5,58 +5,6 @@ namespace Identity.Api;
 
 public static class Config
 {
-    // public static IEnumerable<IdentityResource> IdentityResources =>
-    //     new IdentityResource[]
-    //     {
-    //         new IdentityResources.OpenId(),
-    //         new IdentityResources.Profile(),
-    //     };
-
-    // public static IEnumerable<ApiScope> ApiScopes =>
-    //     new ApiScope[]
-    //     {
-    //         new ApiScope("bitedavi.api", "bitedavi api"),
-    //     };
-
-    // public static IEnumerable<Client> Clients =>
-    //     new Client[]
-    //     {
-    //         // machine to machine client
-    //         new Client
-    //         {
-    //             ClientId = "client",
-    //             ClientSecrets = { new Secret("secret".Sha256()) },
-
-    //             AllowedGrantTypes = GrantTypes.ClientCredentials,
-    //             // scopes that client has access to
-    //             AllowedScopes = { "bitedavi.api" }
-    //         },
-
-    //         // interactive ASP.NET Core Web App
-    //         new Client
-    //         {
-    //             ClientId = "web",
-    //             ClientSecrets = { new Secret("secret".Sha256()) },
-
-    //             AllowedGrantTypes = GrantTypes.Code,
-                    
-    //             // where to redirect to after login
-    //             RedirectUris = { "https://localhost:5002/signin-oidc" },
-
-    //             // where to redirect to after logout
-    //             PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
-                
-    //             AllowOfflineAccess = true,
-                
-    //             AllowedScopes = new List<string>
-    //             {
-    //                 IdentityServerConstants.StandardScopes.OpenId,
-    //                 IdentityServerConstants.StandardScopes.Profile,
-    //                 "bitedavi.api",
-    //             }
-    //         }
-    //     };
-   
     public static IEnumerable<IdentityResource> IdentityResources =>
         new IdentityResource[]
         {
